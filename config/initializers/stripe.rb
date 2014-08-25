7ee0192760aa53deabcdf8ee3542e7b2918feaf6
Cullen Jett <cullenjett@gmail.com>
2014-08-25 11:44:14 -0600

    Fix Stripe webhook

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,8 +2,7 @@ Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
 StripeEvent.configure do |events|
   events.subscribe 'charge.succeeded' do |event|
-    require 'pry'; binding.pry
-    user = User.where(customert_token: event.data.object.customer)
-    Payment.create!(user: user)
+    user = User.where(customer_token: event.data.object.customer).first
+    Payment.create(user: user, amount: event.data.object.amount, reference_id: event.data.object.id)
   end
 end
--- spec/requests/create_payment_on_successful_charge_spec.rb
+++ spec/requests/create_payment_on_successful_charge_spec.rb
@@ -3,29 +3,29 @@ require 'rails_helper'
 describe 'Create payment on successful charge' do
   let(:event_data) do
     {
-      "id" => "evt_14CrCP4OlWR9sB6HLFDsNf8i",
-      "created" => 1404559161,
+      "id" => "evt_14VQel4Qh7wz3HEYL1EkWXoz",
+      "created" => 1408985363,
       "livemode" => false,
       "type" => "charge.succeeded",
       "data" => {
         "object" => {
-          "id" => "ch_14CrCP4OlWR9sB6HJC30Xr9d",
+          "id" => "ch_14VQek4Qh7wz3HEYFfTdbrTg",
           "object" => "charge",
-          "created" => 1404559161,
+          "created" => 1408985362,
           "livemode" => false,
           "paid" => true,
           "amount" => 999,
-          "currency" => "cad",
+          "currency" => "usd",
           "refunded" => false,
           "card" => {
-            "id" => "card_14CrCO4OlWR9sB6Hyc6fuAbE",
+            "id" => "card_14VQeg4Qh7wz3HEYkNiveG1V",
             "object" => "card",
             "last4" => "4242",
             "brand" => "Visa",
             "funding" => "credit",
-            "exp_month" => 10,
-            "exp_year" => 2016,
-            "fingerprint" => "LdQx4SlIvRodhqeR",
+            "exp_month" => 8,
+            "exp_year" => 2014,
+            "fingerprint" => "sO2ZTBxeuCANiBGv",
             "country" => "US",
             "name" => nil,
             "address_line1" => nil,
@@ -37,42 +37,45 @@ describe 'Create payment on successful charge' do
             "cvc_check" => "pass",
             "address_line1_check" => nil,
             "address_zip_check" => nil,
-            "customer" => "cus_4LYJBGmYtTyEIn"
+            "customer" => "cus_4ek9WG5viPn0DB"
           },
           "captured" => true,
           "refunds" => {
             "object" => "list",
             "total_count" => 0,
             "has_more" => false,
-            "url" => "/v1/charges/ch_14CrCP4OlWR9sB6HJC30Xr9d/refunds",
-            "data" => []
+            "url" => "/v1/charges/ch_14VQek4Qh7wz3HEYFfTdbrTg/refunds",
+            "data" => [
+
+            ]
           },
-          "balance_transaction" => "txn_14CrCP4OlWR9sB6Hs7uuPNcD",
+          "balance_transaction" => "txn_14VQek4Qh7wz3HEYlK4yVlPl",
           "failure_message" => nil,
           "failure_code" => nil,
           "amount_refunded" => 0,
-          "customer" => "cus_4LYJBGmYtTyEIn",
-          "invoice" => "in_14CrCP4OlWR9sB6HOSqx396u",
+          "customer" => "cus_4ek9WG5viPn0DB",
+          "invoice" => "in_14VQek4Qh7wz3HEYIavY0rYP",
           "description" => nil,
           "dispute" => nil,
-          "metadata" => {},
-          "statement_description" => nil,
+          "metadata" => {
+          },
+          "statement_description" => "MyFlix",
           "receipt_email" => nil
         }
       },
       "object" => "event",
       "pending_webhooks" => 1,
-      "request" => "iar_4LYJj814spNuWR"
+      "request" => "iar_4ek9Z4SLK8HyYL"
     }
   end
 
-  it "creates a payment with the webhook from stripe for charge.succeeded", :vcr do
+  it "creates a payment with the webhook from stripe for charge.succeeded",  :vcr do
     post "/stripe_events", event_data
     expect(Payment.count).to eq(1)
   end
 
   it "creates the payment associated with the user", :vcr do
-    alice = Fabricate(:user, customer_token: "cus_4ZrMlQOnMeD5UZ")
+    alice = Fabricate(:user, customer_token: "cus_4ek9WG5viPn0DB")
     post "/stripe_events", event_data
     expect(Payment.first.user).to eq(alice)
   end
